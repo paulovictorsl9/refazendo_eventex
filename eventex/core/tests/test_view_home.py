1ca91283cb13d21830d69d56a70237e0a61f649d
Paulo Victor Fonseca Costa <paulovictorsl9@hotmail.com>
2023-02-17 20:50:49 -0300

    Palestrantes

--- eventex/core/tests/test_view_home.py
+++ eventex/core/tests/test_view_home.py
@@ -16,3 +16,19 @@ class HomeTest(TestCase):
         expected = 'href="{}"'.format(r('subscriptions:new'))
         self.assertContains(self.response, expected)
 
+    def testa_palestrantes(self):
+        """deve mostrar palestrantes"""
+        contents = ['Grace Hopper',
+                    'https://www.timeforkids.com/wp-content/uploads/2020/08/Grace_003.jpg?w=926',
+                    'Alan Turing',
+                    'https://cdn.britannica.com/81/191581-050-8C0A8CD3/Alan-Turing.jpg'
+                    ]
+        for expected in contents:
+            with self.subTest():
+                self.assertContains(self.response, expected)
+
+    def testa_link_palestrantes(self):
+        expected = 'href="{}#speakers"'.format(r('home'))
+        self.assertContains(self.response, expected)
+
+
--- eventex/core/views.py
+++ eventex/core/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'index.html')
+    speakers = [
+        {'name': 'Grace Hopper', 'photo': 'https://www.timeforkids.com/wp-content/uploads/2020/08/Grace_003.jpg?w=926'},
+        {'name': 'Alan Turing', 'photo': 'https://cdn.britannica.com/81/191581-050-8C0A8CD3/Alan-Turing.jpg'},
+    ]
+    return render(request, 'index.html', {'speakers': speakers})
+
+
